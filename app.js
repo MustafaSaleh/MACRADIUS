7fd5d48515c1403b561dc8ed85f98c123efaf7da
Moustafa <g-m123@hotmail.com>
2021-08-19 00:05:00 +0300

    add all together, add toaster to capture mac

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 require('dotenv').config()
 var sqlite3 = require('sqlite3').verbose();
+
+//radius
+var radius = require('radius');
+var dgram = require("dgram");
+
+//express
 var express = require('express');
 const sessions = require('express-session');
 var bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
+// create express app
+var app = express();
 var http = require('http')
 var path = require('path');
-var app = express();
+
+
+var server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+const port = 4093
+
+
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-const oneDay = 1000 * 60 * 60 * 24;
 
+
+const oneDay = 1000 * 60 * 60 * 24;
 //session middleware
 app.use(sessions({
     secret: process.env.SESSION_SECRET,
@@ -27,10 +46,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 
-var server = http.createServer(app);
-const port = 4093
 
-var db
+var db = openDB();
 
 function openDB(){
   db = new sqlite3.Database(process.env.DBNAME);
@@ -38,6 +55,18 @@ function openDB(){
   //db.run('DROP TABLE log')
   db.run('CREATE TABLE IF NOT EXISTS log( req TEXT, mac TEXT, visits INTEGER, lastseen TEXT)');
 }
+
+function closeDB(){
+  db.close((err) => {
+      if (err) {
+        console.log('There is some error in closing the database');
+      }else{
+        console.log('Closing the database connection.');
+
+      }
+    });
+}
+
 
 
 app.get('/', function(request, response) {
@@ -59,6 +88,7 @@ app.use(function (req, res, next) {
 
 
 app.get('/home', function(req,res){
+  
     db.all(`SELECT users.rowid, users.name, users.mac, log.lastseen, log.visits FROM users LEFT JOIN log ON users.mac = log.mac`, [], (err, clients) => {
     if (err) {
               console.log(err);
@@ -94,8 +124,7 @@ app.get('/home', function(req,res){
         }
         data +="<table>"
         
-        rows.forEach((row) => {
-             console.log(row.name);
+        rows.forEach((row) => {             
              data +="<tr><td>"+row['rowid']+"</td><td>"+ row['req']+"</td><td>"+ row['mac']+"</td><td>"+ row['visits']+"</td></tr>"
 
         });
@@ -222,6 +251,10 @@ app.get('/home', function(req,res){
 
 
   app.get('/logout', function(req, res){
+    console.log('Logout');
+    req.session.destroy(null)
+    res.redirect("/"); 
+    /*
     db.close((err) => {
       if (err) {
         console.log('There is some error in closing the database');
@@ -233,9 +266,166 @@ app.get('/home', function(req,res){
       }
       
     });
+    */
 
    });
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+
+//start RADIUS
+function saveLog(req,clientMAC){
+
+  db.serialize(()=>{
+    db.get('SELECT rowid, * from users where mac = ?', [clientMAC], function(e,row){
+            if(row == undefined){
+              io.sockets.emit("newClient", clientMAC);
+            }
+        });
+
+      db.get('SELECT rowid, * from log where mac = ?', [clientMAC], function(e,row){
+       
+              if(row !== undefined){
+                      //var v = row.visits +1;
+                      db.run('update log SET visits=?, lastseen=datetime("now") WHERE rowid=? ', [ ++row.visits ,row.rowid], function(err) {
+                          if (err) {
+                              onsole.log(err.message);
+                          }
+                          console.log("update visits for "+ row.mac+": " + row.visits++);
+                          });
+                         // closeDB();
+              }else{
+                
+                  db.run('INSERT INTO log(req,mac,visits,lastseen) VALUES(?,?,1, datetime("now"))', [req,clientMAC], function(err) {
+                      if (err) {
+                          console.log(err.message);
+                      }
+                          console.log("New user has been added");
+                      })
+                     // closeDB();
+                 
+              }
+              
+      })
+      
+
+  });
+
+}
+
+function RadiusServer(settings) {
+  this.config = settings || {};
+  this.port = this.config.port || 1645;
+  this.secret = this.config.secret || "";
+  this.server = null;
+
+  this.ACCESS_REQUEST = 'Access-Request';
+  this.ACCESS_DENIED = 'Access-Reject';
+  this.ACCESS_ACCEPT = 'Access-Accept';
+};
+
+RadiusServer.prototype.start = function () {
+  var self = this;
+
+  // create the UDP server
+  self.server = dgram.createSocket("udp4");
+   
+  self.server.on('message', function (msg, rinfo) {
+      openDB();
+      if (msg && rinfo) {
+
+          // decode the radius packet
+          var packet;
+          console.log(self.secret)
+          console.log(self.msg)
+          try {
+              packet = radius.decode({ packet: msg, secret: self.secret });
+          }
+          catch (err) {
+              console.log('Unable to decode packet.');
+              return;
+          }
+ 
+          // if we have an access request, then
+          if (packet && packet.code == self.ACCESS_REQUEST) {
+             console.log(packet.attributes['User-Name'])  
+              console.log( packet.attributes)
+
+              // get user/password from attributes
+              var username = packet.attributes['User-Name'];
+              var password = packet.attributes['User-Password'];
+
+              // verify credentials, make calls to 3rd party services, then set RADIUS response
+            
+
+              db.serialize(()=>{
+                  db.get('SELECT * FROM users WHERE mac =?', username, function(erro,row){  
+                  var responseCode = self.ACCESS_DENIED;
+                    if(erro){
+                      return console.error(erro.message);
+                    }
+                    
+                  if(row !== undefined){
+                      responseCode = self.ACCESS_ACCEPT;
+                      console.log(`${row.name}`)
+                      console.log("Entry displayed successfully");
+                                      // build the radius response
+                          saveLog(JSON.stringify(packet.attributes),packet.attributes['User-Name'])           
+                      }else{
+                          saveLog(JSON.stringify(packet.attributes),packet.attributes['User-Name'])
+                      }
+
+                      var response = radius.encode_response({
+                          packet: packet,
+                          code: responseCode,
+                          secret: self.secret
+                      });
+                                  // send the radius response
+                                  self.server.send(response, 0, response.length, rinfo.port, rinfo.address, function (err, bytes) {
+                                      if (err) {
+                                          console.log('Error sending response to ', rinfo);
+                                          console.log(err);
+                                      }
+                                  });
+
+                       console.log('Access-Request for "' + username + '" (' + responseCode + ').');    
+                  });
+                 
+               });
+
+          }
+      }
+  });
+   
+  self.server.on('listening', function () {
+      var address = self.server.address();
+      console.log('Radius server listening on port ' + address.port);
+  });
+   
+  self.server.bind(self.port);
+};
+
+var rServer = new RadiusServer({server: "0.0.0.0", port: process.env.RADIUS_PORT, secret: process.env.RADIUS_SECRET });
+rServer.start();
+
+
+//end RADIUS
+
+
+// socket listening
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('newClient', (msg) => {
+    io.emit('newClient', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+
+
+  server.listen(port, () => {
+    console.log(`Admin is running at http://yourip:${port}`)
   })
